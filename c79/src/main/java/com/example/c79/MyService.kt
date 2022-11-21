7d20b3b6b527747ed1bb0b99af16545950e6073f
= <thdgml322@seoultech.ac.kr>
2022-11-21 15:18:29 +0900

    79강. JobScheduler

---
+++ c79/src/main/java/com/example/c79/MyService.kt
@@ -0,0 +1,28 @@
+package com.example.c79
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+
+class MyService : JobService() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("kkang", "MyService...onCreate...")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("kkang", "MyService...onDestroy..")
+    }
+
+    override fun onStartJob(p0: JobParameters?): Boolean {
+        Log.d("kkang", "MyService...onStartJob...")
+        return false
+    }
+
+    override fun onStopJob(p0: JobParameters?): Boolean {
+        Log.d("kkang", "MyService...onStopJob...")
+        return false
+    }
+}
