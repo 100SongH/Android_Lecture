d8be051254f2fef3d3acb8872911487a3091abd9
100SongH <thdgml322@seoultech.ac.kr>
2022-12-22 13:42:15 +0900

    94강. Toolbar

---
+++ c94/src/main/java/com/example/c94/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.c94
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import com.example.c94.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+}
